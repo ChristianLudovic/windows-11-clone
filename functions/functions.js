04da2a8f116e3b9ddb1dfeaa15e3ab3b668b0151
ChristianLudovic <talchrist10@gmail.com>
2024-03-19 21:49:21 +0300

    start menu setted

--- functions/functions.js
+++ functions/functions.js
@@ -4,7 +4,23 @@ const windowsIcon = document.querySelector('.windows-icon');
 const startMenu = document.querySelector('.start-menu');
 
 export function openStartMenu() {
-    windowsIcon.addEventListener('click', () => {
-        startMenu.classList.add('active');
-    });
+
+    let clickCount = 0;
+
+    if (windowsIcon) {
+        windowsIcon.addEventListener('click', () => {
+            clickCount++;
+            if (clickCount === 1) {
+                startMenu.classList.add('active');
+                windowsIcon.classList.add('windows-icon-active');
+            } else if (clickCount === 2) {
+                startMenu.classList.remove('active');
+                windowsIcon.classList.remove('windows-icon-active');
+                clickCount = 0;
+            }
+        });
+    }
+
+
+    
 }
